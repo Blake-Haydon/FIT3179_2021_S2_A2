c2a1af151e987071dc48c3f447edf4e431322945
Blake Haydon <blake.a.haydon@gmail.com>
2021-09-16 12:25:54 +1000

    Save point

--- docs/index.js
+++ docs/index.js
@@ -21,4 +21,44 @@ var yourVlSpec = {
     }
 };
 
+var yourVlSpec2 = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    description: 'A simple bar chart with embedded data.',
+    data: {
+        values: [
+            { a: 'A', b: 28 },
+            { a: 'B', b: 55 },
+            { a: 'H', b: 87 },
+            { a: 'I', b: 52 }
+        ]
+    },
+    mark: 'bar',
+    encoding: {
+        x: { field: 'a', type: 'ordinal' },
+        y: { field: 'b', type: 'quantitative' }
+    }
+};
+
 vegaEmbed('#vis', yourVlSpec);
+
+
+
+
+
+let waypoint = new Waypoint({
+    element: document.getElementById('test-thing'),
+    handler: function (direction) {
+        console.log(this.id + ' hit')
+        vegaEmbed('#vis', yourVlSpec);
+
+    }
+})
+
+let waypoint2 = new Waypoint({
+    element: document.getElementById('test-thing-2'),
+    handler: function (direction) {
+        console.log(this.id + ' hit')
+        vegaEmbed('#vis', yourVlSpec2);
+    }
+})
+
