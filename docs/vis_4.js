bbe15eb6aa4c829ff193c838b4bd1dcaeece4ee2
Blake Haydon <blake.a.haydon@gmail.com>
2021-10-12 10:08:18 +1100

    Add basic heat map

--- docs/vis_4.js
+++ docs/vis_4.js
@@ -1,26 +1,39 @@
 const VlSpec4 = {
     $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-    description: 'A simple bar chart with embedded data.',
+    title: 'NBN Speeds',
     data: {
-        values: [
-            { a: 'A', b: 28 },
-            { a: 'B', b: 55 },
-            { a: 'C', b: 43 },
-            { a: 'D', b: 91 },
-            { a: 'E', b: 81 },
-            { a: 'F', b: 53 },
-            { a: 'G', b: 19 },
-            { a: 'H', b: 87 },
-            { a: 'I', b: 52 }
-        ]
+        url: "data/NBN_speeds/concat_nbn_speed_data.csv",
+        format: {
+            type: "csv"
+        }
     },
-    mark: 'bar',
+    mark: 'rect',
     encoding: {
-        x: { field: 'a', type: 'ordinal' },
-        y: { field: 'b', type: 'quantitative' }
+        x: {
+            field: 'Time',
+            type: 'ordinal',
+            sort: [
+                "1:00 am", "2:00 am", "3:00 am", "4:00 am", "5:00 am", "6:00 am",
+                "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 am",
+                "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm",
+                "7:00 pm", "8:00 pm", "9:00 pm", "10:00 pm", "11:00 pm", "12:00 pm",
+            ]
+        },
+        y: {
+            field: 'Date',
+            type: 'ordinal',
+        },
+        color: { field: 'Speed Difference', type: 'quantitative' },
+    },
+    config: {
+        axis: {
+            grid: true,
+            tickBand: "extent"
+        },
     }
+
 };
 
-vegaEmbed('#vis4', VlSpec3);
+vegaEmbed('#vis4', VlSpec4);
 
 
